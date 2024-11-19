6f4a2a13858c417df8f2004f9a1dc8ca47df261b
シミズ <kurumishimizu24@gmail.com>
2024-11-19 18:40:03 +0900

    a

--- script.js
+++ script.js
@@ -80,3 +80,82 @@ $(function () {
     hAnimate();
     vAnimate();
 });
+
+// スクロールイベント
+window.addEventListener('scroll', () => {
+    // visualVerticalImgのid名がつく画像を取得
+    const elemImg = document.getElementById('visualVerticalImg');
+    //現在のスクロール位置を取得して10で除算
+    let imgVerScrollY = window.scrollY / 10;
+    //取得したスクロール値でY値をtransform
+    elemImg.style.transform = 'translateY(' + 0 + imgVerScrollY + 'px) scale(1.4)';
+});
+
+// var rellax = new Rellax('.rellax');
+
+luxy.init({
+    wrapper: '#luxy',
+    wrapperSpeed: 0.09, // スクロール速度の調整（デフォルト値は0.08）
+});
+
+
+$(function () {
+
+    //カーソル要素の指定
+    var cursor = $("#cursor");
+    //ちょっと遅れてついてくるストーカー要素の指定
+    var stalker = $("#stalker");
+
+    //mousemoveイベントでカーソル要素を移動させる
+    $(document).on("mousemove", function (e) {
+        //カーソルの座標位置を取得
+        var x = e.clientX;
+        var y = e.clientY;
+        //カーソル要素のcssを書き換える用
+        cursor.css({
+            "opacity": "1",
+            "top": y + "px",
+            "left": x + "px"
+        });
+        //ストーカー要素のcssを書き換える用
+        setTimeout(function () {
+            stalker.css({
+                "opacity": "1",
+                "top": y + "px",
+                "left": x + "px"
+            });
+        }, 0);//カーソルより遅れる時間を指定
+    });
+    //aタグホバー
+    $("a").on({
+        "mouseenter": function () {
+            //activeクラス付与
+            cursor.addClass("active");
+            stalker.addClass("active");
+        },
+        "mouseleave": function () {
+            cursor.removeClass("active");
+            stalker.removeClass("active");
+
+        }
+    });
+});
+
+(function () {
+    // 設定
+    var interval = 5000; // 切り替わりの間隔（ミリ秒）
+    var fade_speed = 1000;// フェード処理の早さ（ミリ秒）
+    $(".storyImg img").hide();
+    $(".storyImg img:first").addClass("active").show();
+
+    var changeImage = function () {
+        var $active = $(".storyImg img.active");
+        var $next = $active.next("img").length ? $active.next("img") : $(".storyImg img:first");
+
+        $active.fadeOut(fade_speed).removeClass("active");
+        $next.fadeIn(fade_speed).addClass("active");
+    }
+
+
+    setInterval(changeImage, interval);
+}());
